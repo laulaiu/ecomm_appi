d49df9d42fc5b91930553df256d0170eca17f9fd
Leonardo Santos <laulaiu14@gmail.com>
2021-06-14 17:24:55 -0300

    Adicionar arquivos de projeto.

---
+++ E-commerce/Controllers/controle_webController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_commerce.Controllers
+{
+    public class controle_webController : Controller
+    {
+
+        // GET: controle_web
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+    }
+}
