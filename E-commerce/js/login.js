b59a9d8e3576d477c64bf3fdac638ce3ba938d80
Leonardo Santos <laulaiu14@gmail.com>
2021-06-17 21:37:29 -0300

    pagina de login finalizado

---
+++ E-commerce/js/login.js
@@ -0,0 +1,10 @@
+﻿function mostrar() {
+    document.getElementById("ocultar1").hidden = false;
+    document.getElementById("div_pr").style.height = "480px";
+
+}
+
+function ocultar() {
+    document.getElementById("ocultar1").hidden = true;
+    document.getElementById("div_pr").style.height = "400px";
+}
